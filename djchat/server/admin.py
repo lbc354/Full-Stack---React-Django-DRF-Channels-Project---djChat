98d38333da9d9c16d70b76122ac3db2c7abaed30
Lucas Barros <lucas.carregozi@dnit.gov.br>
2025-07-28 15:32:52 -0300

    17. Build: Chat Server Administration Models (Database Tables and Fields)

--- djchat/server/admin.py
+++ djchat/server/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from server.models import Category, Server, Channel
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Server)
+admin.site.register(Channel)
