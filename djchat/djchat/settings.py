36f14c93c35d1810292ddcd23410b25b0a879bd2
Lucas Barros <lucas.carregozi@dnit.gov.br>
2025-08-21 15:41:55 -0300

    19. Build: Configuring Default Authentication Classes in Django DRF

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -99,6 +99,9 @@ AUTH_USER_MODEL = "account.Account"
 # drf-spectacular
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ]
 }
 SPECTACULAR_SETTINGS = {
     "TITLE": "Your Project API",
