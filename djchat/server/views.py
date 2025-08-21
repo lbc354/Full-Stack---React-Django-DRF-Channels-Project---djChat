c02f6699cc11e144491a7d53d660c0e49d8a8b8e
Lucas Barros <lucas.carregozi@dnit.gov.br>
2025-08-21 17:17:47 -0300

    22. Build: Creating an API Endpoint for Filtering Servers by Server ID

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from server.models import Server
 from server.serializer import ServerSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 
 
 class ServerListViewSet(viewsets.ViewSet):
@@ -11,14 +12,30 @@ class ServerListViewSet(viewsets.ViewSet):
     def list(self, request):
         category = request.query_params.get("category")
         by_user = request.query_params.get("by_user") == "true"
+        by_server_id = request.query_params.get("by_server_id")
         qty = request.query_params.get("qty")
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            else:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+
+        if by_server_id:
+            try:
+                self.queryset = self.queryset.filter(id=by_server_id)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_server_id} not found."
+                    )
+            except ValueError:
+                raise ValidationError(
+                    detail=f"Server id {by_server_id} is not an integer."
+                )
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
